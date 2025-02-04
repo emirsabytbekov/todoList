b2da4cf049fd7c5b83456c1219321b95f422b2cb
emir.sabytbekov <emir.sabytbekov07@gmail.com>
2025-02-04 16:54:18 +0600

    add logic for addTask function

--- script.js
+++ script.js
@@ -9,19 +9,28 @@ const deleteButton = document.getElementById("deleteButton");
 
 // Initialize
 document.addEventListener("DOMContentLoaded", function () {
-    addButton.addEventListener('click', addTask());
+    addButton.addEventListener('click', addTask);
     todoInput.addEventListener('keydown', function (event) {
         if (event.key === 'Enter') {
             event.preventDefault(); 
             addTask();
         }
     });
-    deleteButton.addEventListener('click', deleteAllTasks());
+    deleteButton.addEventListener('click', deleteAllTasks);
     displayTasks();
 });
 
 function addTask () {
-
+    const newTask = todoInput.value.trim();
+    if (newTask !== "") {
+        todo.push({
+            text: newTask,
+            disabled: false,
+        });
+        saveToLocalStorage();
+        todoInput.value = "";
+        displayTasks();
+    }
 }
 
 function deleteAllTasks () {
@@ -30,4 +39,8 @@ function deleteAllTasks () {
 
 function displayTasks () {
     
+}
+
+function saveToLocalStorage () {
+    localStorage.setItem('todo', JSON.stringify(todo));
 }
