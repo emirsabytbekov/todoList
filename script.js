9dd98dc85a442937f2cd0c7ef870f65af07e0082
emir.sabytbekov <emir.sabytbekov07@gmail.com>
2025-02-04 16:29:42 +0600

    set up DOM elements

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 // Retrieve todo from a local storage or initialize an empty array
 
 let todo = JSON.parse(localStorage.getItem('todo')) || [];
+const todoInput = document.getElementById("todoInput");
+const todoList = document.getElementById("todoList");
+const todoCount = document.getElementById("todoCount");
+const todoButton = document.querySelector(".btn");
+const deleteButton = document.getElementById("deleteButton");
+
