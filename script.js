50fb6a9712b372a46a60fabebdb933f3d09914cb
emir.sabytbekov <emir.sabytbekov07@gmail.com>
2025-02-05 22:41:37 +0600

    add editTask function

--- script.js
+++ script.js
@@ -54,6 +54,25 @@ function displayTasks () {
     }); 
 }
 
+function editTask(index) {
+    const todoItem = document.getElementById(`todo-${index}`);
+    const existingText = todo[index].text;
+    const inputElement = document.createElement("input");
+
+    inputElement.value = existingText;
+    todoItem.replaceWith(inputElement);
+    inputElement.focus();
+
+    inputElement.addEventListener("blur", function () {
+        const updatedText = inputElement.value.trim();
+        if (updatedText) {
+            todo[index].text = updatedText;
+            saveToLocalStorage();
+        }
+        displayTasks();
+    });
+}
+
 function toggleTask(index) {
     todo[index].disabled = !todo[index].disabled;
     saveToLocalStorage();
